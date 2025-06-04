1ed1c227764836f91435a97820be13c645a3040e
Edward Taylor <brizzalnt@gmail.com>
2025-06-04 18:39:03 +0100

    shapes added

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ struct PixelData{
     x: f64,
     y: f64,
     z: f64,
-    
+    shape : String,
 }
 
 fn main() {
@@ -38,6 +38,6 @@ fn main() {
         }
     }
 
-    let mut _pixel = PixelData{ alpha : 255, red: 0, green: 0, blue:0 , emit:0 , shine: 0,  x:0.0 , y:0.0 , z:0.0};
+    let mut _pixel = PixelData{ alpha : 255, red: 0, green: 0, blue:0 , emit:0 , shine: 0,  x:0.0 , y:0.0 , z:0.0, shape:"pixel".to_string()};
     println!("{:?}", _pixel);
 }
