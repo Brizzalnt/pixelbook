8a0a1d750b168d84d8a39e6ffcffc08d060ed4af
Open Ideas. <Brizzalnt@gmail.com>
2025-06-06 11:28:26 +0100

    Update files.rs - ideas for file format

--- src/files.rs
+++ src/files.rs
@@ -25,16 +25,42 @@ pub fn run() {
     let separator = ','; 
 
 
-    let mut indexer = 0;
+    let mut indexer:usize = 0;
+    
     match read_file_into_vec_by_separator(filepath, separator) {
         Ok(data) => {
             println!("Contents of the file, split by '{}':", separator);
             for (i, item) in data.iter().enumerate() {
-                print!("{}: \"{}\"", i, item);
+                let mut labeler: String;
+                let x2 = String::from("x");
+                let y2: String = String::from("y");
+                let z2 = String::from("z");
+                let c2 = String::from("cyan");
+                let m2: String = String::from("magenta");
+                let y3 = String::from("yellow");
+                let k2 = String::from("black");
+                let a2: String = String::from("alpha");
+                let end: String = String::from("!");
+                
+                
+
+            
                 indexer +=1;
-                if indexer > 7{
+                match indexer{
+                    1 => labeler = x2.to_string(),
+                    2 => labeler = y2.to_string(),
+                    3 => labeler = z2.to_string(),
+                    4 => labeler = c2.to_string(),
+                    5 => labeler = m2.to_string(),
+                    6 => labeler = y3.to_string(),
+                    7 => labeler = k2.to_string(),
+                    8 => labeler = a2.to_string(),
+                    _ => labeler = end.to_string()
+                }
+                print!("{}: \"{}\"", labeler, item);
+                if indexer == 8{
                     indexer = 0;
-                    println!("pixel end.")
+                    println!("")
                 }
             }
         }
